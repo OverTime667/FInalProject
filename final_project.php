59be307a22a3db1d495d80e11738c9fe65f04af9
RajiNas <60361637+RajiNas@users.noreply.github.com>
2021-04-09 22:57:00 -0400

    navbar

--- final_project.php
+++ final_project.php
@@ -18,10 +18,60 @@
     <link rel="stylesheet" href="style.css" type="text/css">
 </head>
 <body>
+    <!--div representing the header of the page --> 
     <div id="headerDiv">
+        <!-- logo part of headerDiv -->
+        <div id="logo">
+            <h2><b>Logo</b></h2>
+        </div>
 
-        hello
+        <!-- search part of the headerdiv -->
+        <div id="searchpart">
+        <nav class="navbar navbar-light bg-light">
+            <div class="container-fluid">
+                <form class="d-flex">
+                <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+                <button class="btn btn-outline-success" type="submit">Search</button>
+                </form>
+            </div>
+            </nav>
+        </div>
 
+        <!-- user information part of headerDiv -->
+        <div id="currentUserInfo"> 
+            user
+        </div>
+
+    </div>
+
+
+    <!-- navBar header page section -->
+    <div>
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+            <div class="container-fluid">
+                
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavDropdown">
+                    <ul class="navbar-nav">
+                        <li class="nav-item ">
+                            <a class="nav-link active" aria-current="page" href="final_project.php">Home</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Listing</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Promotions</a>
+                        </li>
+                    </ul>                        
+                    <div id="registerationDiv">                        
+                        <a href="#"> Register</a> or <a href="#"> Sign in</a>                        
+                    </div>                                             
+                    
+                </div>
+            </div>
+        </nav>
     </div>
 
 </body>   
