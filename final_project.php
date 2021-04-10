9c0d5510aef88269a6130647bd18a246108f9997
RajiNas <60361637+RajiNas@users.noreply.github.com>
2021-04-09 22:10:53 -0400

    new

--- final_project.php
+++ final_project.php
@@ -1,13 +1,10 @@
-<<<<<<< HEAD
 <?php
 
 
 ?>
 
 
-=======
-<?php  ?>
->>>>>>> 16c7541c3eda702d0042ca5b3192f0429d362744
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -17,24 +14,15 @@
     <title>Document</title>
     <!--implement bootstrapp -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-<<<<<<< HEAD
-=======
 
->>>>>>> 16c7541c3eda702d0042ca5b3192f0429d362744
     <link rel="stylesheet" href="style.css" type="text/css">
 </head>
 <body>
     <div id="headerDiv">
-<<<<<<< HEAD
+
         hello
 
     </div>
-</body>
-</html>
-=======
 
-
-    </div>
-</body>
+</body>   
 </html>
->>>>>>> 16c7541c3eda702d0042ca5b3192f0429d362744
