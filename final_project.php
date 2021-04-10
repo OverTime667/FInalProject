16c7541c3eda702d0042ca5b3192f0429d362744
OverTime667 <60228258+OverTime667@users.noreply.github.com>
2021-04-09 22:03:46 -0400

    it works

--- final_project.php
+++ final_project.php
@@ -1,18 +1,4 @@
-<?php
-
-
-echo 'Hello Joel';
-
-echo 'hello raji I am here';
-
-<<<<<<< Updated upstream
-echo 'hello ';
-
-
-?>
-=======
-?>
-
+<?php  ?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -22,14 +8,13 @@ echo 'hello ';
     <title>Document</title>
     <!--implement bootstrapp -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/js/bootstrap.bundle.min.js" integrity="sha384-JEW9xMcG8R+pH31jmWH6WWP0WintQrMb4s7ZOdauHnUtxwoG2vI5DkLtS3qm9Ekf" crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="css/style.css" type="text/css">
+
+    <link rel="stylesheet" href="style.css" type="text/css">
 </head>
 <body>
-    <div>
+    <div id="headerDiv">
 
 
     </div>
 </body>
 </html>
->>>>>>> Stashed changes
