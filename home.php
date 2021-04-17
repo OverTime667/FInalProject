0f9bc37c9221aba31574e096d2e696984bf35137
RajiNas <60361637+RajiNas@users.noreply.github.com>
2021-04-17 13:09:52 -0400

    masterpage

--- home.php
+++ home.php
@@ -1,6 +1,4 @@
-<?php
 
-?>
 
 
 <!DOCTYPE html>
@@ -9,7 +7,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Promotions</title>
+    <title>Document</title>
     <!--implement bootstrapp -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
 
@@ -19,22 +17,20 @@
 <?php
 
 include_once("header.php");
+
 ?>
 
 
     <!-- Body of page Starts here -->
     <div id="bodyPage">
 
-    Promotions page
+    Home
 
     </div>
+ <?php
 
-<?php
-
-    include_once("footer.php");
-?>
-  
-  
+include_once("footer.php");
+?>  
 
 </body>   
 </html>
---
+++ listing.php
@@ -0,0 +1,38 @@
+<?php
+
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Listing</title>
+    <!--implement bootstrapp -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css" type="text/css">
+</head>
+<body>
+<?php
+
+include_once("header.php");
+
+?>
+
+
+    <!-- Body of page Starts here -->
+    <div id="bodyPage">
+
+    Listing page
+
+    </div>
+<?php
+
+include_once("footer.php");
+?>   
+
+</body>   
+</html>
