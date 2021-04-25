0d3b6af2fb722721ba4c6f4e0bb90b21bdeb36f0
RajiNas <60361637+RajiNas@users.noreply.github.com>
2021-04-25 17:20:48 -0400

    posts

--- post.php
+++ post.php
@@ -26,22 +26,36 @@
 
         public function insertPost(){
 
+            $owner = $this-> con-> real_escape_string($_POST['owner']);
+            $brand = $this-> con-> real_escape_string($_POST['brand']);
+            $price = $this-> con-> real_escape_string($_POST['price']);
+            $location = $this-> con-> real_escape_string($_POST['location']);
+            $milage = $this-> con-> real_escape_string($_POST['milage']);
+            $seats = $this-> con-> real_escape_string($_POST['seats']);
+            $availability = $this-> con-> real_escape_string($_POST['availability']);
+            $dateofmodel = $this-> con-> real_escape_string($_POST['date_of_model']);
+            $dateofpost = date('d-m-y h:i:s');
+            $fees = $this-> con-> real_escape_string($_POST['fees']);
+            $datesold = $this-> con-> real_escape_string($_POST['dateSold']);
+            $image = $this-> con-> real_escape_string($_POST['image']);
+            $other = $this-> con-> real_escape_string($_POST['other']);
             
+            
+
+            $query = " INSERT INTO posts(owner, brand, price, location, milage, seats, availability, date_of_model, date_of_post, fees, dateSold, image, other)
+            VALUES ('$owner','$brand','$price','$location','$milagen','$seats','$availability', '$dateofmodel', '$dateofpost', '$fees', '$datesold', '$image', '$other')"; 
+           
+            $sql = $this->con->query($query);
+            if($sql == true){
+                echo "Data inserted!";
+            }
+            else{
+                echo "Registration failed, please try again!";
+            }
 
 
 
         }
-
-
-
-
-
-
-
-
-
-
-
     }
 
 
